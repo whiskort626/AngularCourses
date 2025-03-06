d49f70f22f9dd10a49555493801efe1d49b17f5a
Fabian Fernandez <fafca626@hotmail.com>
2025-03-06 16:32:42 -0500

    Se termina la primera APP'1-angular-app[ngif-ngfor]'

--- AngularApps/angular-app/src/app/app.component.ts
+++ AngularApps/angular-app/src/app/app.component.ts
@@ -1,12 +1,25 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
+
 export class AppComponent {
-  public title: string = 'Hola Mundo Angular2!';
+  public title: string = 'Hola Mundo Angular desde Componenete!';
+  public users: string[] = ['Juan', 'Pedro', 'Maria', 'Ana'];
+  private age: number = 30;
+  visible: boolean = false;
+  
+  public setVisible(visible: boolean): void {
+    this.visible = visible;
+    ////Con un operador ternario y no recibiendo parametro quedaria asi 
+    //this.visible =  this.visible ? false : true;  
+    console.log('Visible: ', this.visible);
+  }
+  
 }
